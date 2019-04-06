712832ee0786d89f850bbd4de8ee3ef503774689
Jorg van der Ham <jvdham@gmail.com>
2019-04-06 18:45:37 +0200

    added node mailer

--- routes/message.js
+++ routes/message.js
@@ -3,6 +3,16 @@ const router  = express.Router();
 
 const Message = require('../models/message');
 
+const nodemailer = require('nodemailer')
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAILUSER,
+    password: process.env.EMAILPASS
+  }
+})
+
 // GET /message/all
 router.get('/all', (req, res, next) => {
   Message.find({})
@@ -21,6 +31,12 @@ router.post('/add', (req, res) => {
     body: req.body.body
   })
   .then(message => {
+    transport.sendMail({
+      from: "no-reply <no-reply@ironhack.com>",
+      to: "jorg.vanderham@ironhack.com",
+      subject: "the subject",
+      text: `a new message has been posted: ${message}`
+    })
     res.send(message)
   })
   .catch(err => {
